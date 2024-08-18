2430a9759abd075eb5ab7b7e982d8157edad8730
sumathin <sunatara@microsoft.com>
2024-08-17 18:04:21 -0700

    update newsfeed

--- newsfeed/src/components/Newsfeed.tsx
+++ newsfeed/src/components/Newsfeed.tsx
@@ -1,24 +1,57 @@
 import * as React from "react";
 import Story from "./Story";
 
-export default function Newsfeed() {
-  const story = {
-    title: "Placeholder Story",
-    summary: "Placeholder data, to be replaced with data fetched via GraphQL",
-    poster: {
-      name: "Placeholder Person",
-      profilePicture: {
-        url: "/assets/cat_avatar.png",
-      },
-    },
-    thumbnail: {
-      url: "/assets/placeholder.jpeg",
-    },
-  };
+import { graphql } from "relay-runtime";
+import { useLazyLoadQuery } from "react-relay";
+import type { NewsfeedQuery as NewsfeedQueryType } from "./__generated__/NewsfeedQuery.graphql";
 
+const NewsfeedQuery = graphql`
+  query NewsfeedQuery {
+    topStory {
+      title
+      summary
+      poster {
+        name
+        profilePicture {
+          url
+        }
+      }
+      thumbnail {
+        url
+      }
+    }
+  }
+`;
+
+export default function Newsfeed({}) {
+  const data = useLazyLoadQuery<NewsfeedQueryType>(NewsfeedQuery, {});
+  const story = data.topStory;
+  // As before:
   return (
     <div className="newsfeed">
       <Story story={story} />
     </div>
   );
 }
+
+// export default function Newsfeed() {
+//   const story = {
+//     title: "Placeholder Story",
+//     summary: "Placeholder data, to be replaced with data fetched via GraphQL",
+//     poster: {
+//       name: "Placeholder Person",
+//       profilePicture: {
+//         url: "/assets/cat_avatar.png",
+//       },
+//     },
+//     thumbnail: {
+//       url: "/assets/placeholder.jpeg",
+//     },
+//   };
+
+//   return (
+//     <div className="newsfeed">
+//       <Story story={story} />
+//     </div>
+//   );
+// }
